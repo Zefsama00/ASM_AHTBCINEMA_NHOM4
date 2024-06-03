ec41f2f8d3ba70908424f7d74b98107d8070fb81
DuyDeptry <shitf735@gmail.com>
2024-06-03 17:14:24 +0700

    Fix

---
+++ MVC_ASM_AHTBCinema_NHOM4_SD18301/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_ASM_AHTBCinema_NHOM4_SD18301.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

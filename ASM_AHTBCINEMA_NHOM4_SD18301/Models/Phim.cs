9d3e4e4fe88c54a39dcd5fa55aca86e5305d33af
DuyDeptry <shitf735@gmail.com>
2024-06-06 14:53:40 +0700

    fix

--- ASM_AHTBCINEMA_NHOM4_SD18301/Models/Phim.cs
+++ ASM_AHTBCINEMA_NHOM4_SD18301/Models/Phim.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
 
 namespace AHTBCinema_NHOM4_SD18301.Models
 {
@@ -9,6 +11,7 @@ namespace AHTBCinema_NHOM4_SD18301.Models
         [Key]
         public string IdPhim { get; set; }
         public string TenPhim { get; set; }
+        public Byte Hinhanh { get; set; }
         public string DangPhim { get; set; }
         [ForeignKey("LoaiPhim")]
         public string TheLoai { get; set; }
